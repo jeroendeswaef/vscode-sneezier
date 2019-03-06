94538e9b12c1fe832a3639ad69fb3a7c1327f54c
Jeroen De Swaef <jeroendeswaef@gmail.com>
2019-03-06 21:05:37 +0100

    Experiments with rendering svg directly

--- webview/js/main.js
+++ webview/js/main.js
@@ -8,68 +8,79 @@ function find(qs) {
 
 const vscode = acquireVsCodeApi();
 
-var CurvesView = {
-    oninit: function() {
-        this.lineIndex = startLine;
-        window.addEventListener('message', event => {
+function CurvesView() {
+    let imageWidth;
+    let imageHeight;
 
-            const message = event.data; // The JSON data our extension sent
-        
-            switch (message.command) {
-                case 'selectLine':
-                    this.lineIndex = message.lineIndex;
-                    m.redraw();
-                    break;
-            }
-        });
-    },
-    oncreate: function(vnode) {
-        this.fns = bindDrawFunctions(0, vnode.attrs.imageWidth, vnode.attrs.imageHeight);
-        let curves = [];
-        let previousPaths = initialPaths;
-        initialPaths.forEach((path, i) => curves = curves.concat(convertPath(Bezier, path.svgPath).curves.map(curve => Object.assign(curve, { line: path.line }))));
-        this.drawFns = [];
-        const updateFn = _.debounce(() => {
+    return {
+        oninit: function(vnode) {
+            imageWidth = vnode.attrs.imageWidth;
+            imageHeight = vnode.attrs.imageHeight;
+            this.lineIndex = startLine;
+            window.addEventListener('message', event => {
+
+                const message = event.data; // The JSON data our extension sent
+            
+                switch (message.command) {
+                    case 'selectLine':
+                        this.lineIndex = message.lineIndex;
+                        m.redraw();
+                        break;
+                }
+            });
+        },
+        oncreate: function(vnode) {
+            return;
+            this.fns = bindDrawFunctions(0, vnode.attrs.imageWidth, vnode.attrs.imageHeight);
+            let curves = [];
+            let previousPaths = initialPaths;
+            initialPaths.forEach((path, i) => curves = curves.concat(convertPath(Bezier, path.svgPath).curves.map(curve => Object.assign(curve, { line: path.line }))));
+            this.drawFns = [];
+            const updateFn = _.debounce(() => {
+                for(let i = 0; i < curves.length; i++) {
+                    const curve = curves[i];
+                    const newPath = curve.toSVG()
+                    if (previousPaths[i].svgPath != newPath) {
+                        // vscode.postMessage({
+                        //     command: 'lineChanged',
+                        //     line: i,
+                        //     content: newPath
+                        // })
+                        // previousPaths[i] = newPath;
+                    }
+                }
+            }, 1000);
             for(let i = 0; i < curves.length; i++) {
                 const curve = curves[i];
-                const newPath = curve.toSVG()
-                if (previousPaths[i].svgPath != newPath) {
-                    // vscode.postMessage({
-                    //     command: 'lineChanged',
-                    //     line: i,
-                    //     content: newPath
-                    // })
-                    // previousPaths[i] = newPath;
-                }
+                const draw = () => {
+                    if (curve.line === this.lineIndex) {
+                        this.fns.drawSkeleton(curve);
+                    }
+                    this.fns.setColor("#FF0000");
+                    this.fns.drawCurve(curve);
+                };
+                this.drawFns.push(draw);
+                draw();
+                handleInteraction(this.fns.getCanvas(), curve).onupdate = (evt) => {
+                    if (evt) {
+                        updateFn();
+                        //console.info("handleInteraction", typeof debounce, typeof lodash, typeof _, typeof _.debounce);
+                        this.fns.reset(curve); 
+                        this.drawFns.forEach(draw => draw(evt));
+                    }
+                };
             }
-        }, 1000);
-        for(let i = 0; i < curves.length; i++) {
-            const curve = curves[i];
-            const draw = () => {
-                if (curve.line === this.lineIndex) {
-                    this.fns.drawSkeleton(curve);
-                }
-                this.fns.setColor("#FF0000");
-                this.fns.drawCurve(curve);
-            };
-            this.drawFns.push(draw);
-            draw();
-            handleInteraction(this.fns.getCanvas(), curve).onupdate = (evt) => {
-                if (evt) {
-                    updateFn();
-                    //console.info("handleInteraction", typeof debounce, typeof lodash, typeof _, typeof _.debounce);
-                    this.fns.reset(curve); 
-                    this.drawFns.forEach(draw => draw(evt));
-                }
-            };
+        },
+        onupdate: function(vnode) {
+            this.fns.reset(); 
+            this.drawFns.forEach(draw => draw());
+        },
+        view: function() {
+            //return m("figure")
+            return m("svg", { class: "svg-area", viewBox: `0 0 ${imageWidth} ${imageHeight}`},
+                initialPaths.map(path => m("path", { class: "path-element", onclick: () => { console.info('svg clicked!'); }, d: path.svgPath, fill: "none", 'stroke-width': 2, stroke: "hotpink" }))
+            )
         }
-    },
-    onupdate: function(vnode) {
-        this.fns.reset(); 
-        this.drawFns.forEach(draw => draw());
-    },
-    view: function() {
-        return m("figure")
     }
 }
 
