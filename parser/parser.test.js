1b33a9ede45eb50defb5cd0c743b6deef0fc22ba
Jeroen De Swaef <jeroendeswaef@gmail.com>
2019-06-16 15:38:17 +0200

    Parsing one-line and multi-line expressions

--- parser/parser.test.js
+++ parser/parser.test.js
@@ -2,20 +2,29 @@ const antlr4 = require('antlr4');
 const Lexer = require('./SvgPathLexer').SvgPathLexer;
 const Parser = require('./SvgPathParser').SvgPathParser;
 const Visitor = require('./SvgJsonOutputVisitor').SvgJsonOutputVisitor;
-const Listener = require('./SvgJsonOutputListener').SvgJsonOutputListener;
+// const Listener = require('./SvgJsonOutputListener').SvgJsonOutputListener;
 
-test('Testing a oneline svg expression', () => {
-    var input = "M 10 80 C 40 10 110 41 95 80\n"
-	var chars = new antlr4.InputStream(input);
-	var lexer = new Lexer(chars);
-	var tokens  = new antlr4.CommonTokenStream(lexer);
-	var parser = new Parser(tokens);
-	parser.buildParseTrees = true;
-	var tree = parser.svgPath();
+const getElements = (input) => {
+	const chars = new antlr4.InputStream(input);
+	const lexer = new Lexer(chars);
+	const tokens  = new antlr4.CommonTokenStream(lexer);
+	const parser = new Parser(tokens);
+	//parser.buildParseTrees = true;
+	const tree = parser.svgPath();
 	const visitor = new Visitor();
 	tree.accept(visitor);
-	expect(visitor.elements).toMatchSnapshot();
+	return visitor.elements;
 	//const listener = new Listener();
 	//antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree);
-    //expect(1 + 2).toBe(3);
+}
+
+describe('Testing svg expressions', () => {
+	test('Testing a one-line svg expression', () => {
+		const input = "M 10 80 C 40 10 110 41 95 80";
+		expect(getElements(input)).toMatchSnapshot();
+	});
+	test('Testing a multi-line svg expression', () => {
+		const input = "M 10 80 C 40 10 110 41 95 80\nM 90 100";
+		expect(getElements(input)).toMatchSnapshot();
+	});
 });
